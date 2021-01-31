9a6aa4ffd9d04927209563e89dc2f0d0c55be535
master <58977579+alexmasterblack@users.noreply.github.com>
2021-01-31 16:20:01 +1000

    Adding files

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <ValidationForm></ValidationForm>
+  </div>
+</template>
+
+<script>
+import ValidationForm from '@/components/ValidationForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ValidationForm
+  },
+      loaders: {
+        scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+        sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass"
+      }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
